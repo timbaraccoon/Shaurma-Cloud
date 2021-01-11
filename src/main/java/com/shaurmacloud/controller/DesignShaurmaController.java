c1c41bb8c837a6622472fdfe0b541662c56bb693
Ruslan <timbaraccoon@gmail.com>
2021-01-11 23:36:56 +0500

    ...

---
+++ src/main/java/com/shaurmacloud/controller/DesignShaurmaController.java
@@ -0,0 +1,45 @@
+package com.shaurmacloud.controller;
+
+import com.shaurmacloud.entity.Ingredient;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
+public class DesignShaurmaController {
+
+    @GetMapping
+    public String showDesignForm(Model model) {
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("WHBD", "White Bread", Ingredient.Type.WRAP),
+                new Ingredient("BLBD", "Black Bread", Ingredient.Type.WRAP),
+                new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN),
+                new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN),
+                new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES),
+                new Ingredient("TMTO", "Diced Tomato", Ingredient.Type.VEGGIES),
+                new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE),
+                new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE),
+                new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE),
+                new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE)
+        );
+
+
+        Ingredient.Type[] types = Ingredient.Type.values();
+
+        // TODO
+
+        for (Ingredient.Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(),
+                    filterByType(ingredients, type))
+        }
+    }
+
+
+}
