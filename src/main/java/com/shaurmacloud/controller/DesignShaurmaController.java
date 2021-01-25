be5f784a07ad55c4b841f8504ccbbbceec4e2f7a
Ruslan <timbaraccoon@gmail.com>
2021-01-25 23:41:52 +0500

    ...

--- src/main/java/com/shaurmacloud/controller/DesignShaurmaController.java
+++ src/main/java/com/shaurmacloud/controller/DesignShaurmaController.java
@@ -1,14 +1,20 @@
 package com.shaurmacloud.controller;
 
 import com.shaurmacloud.entity.Ingredient;
+import com.shaurmacloud.entity.Ingredient.Type;
+import com.shaurmacloud.entity.Shaurma;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -18,31 +24,33 @@ public class DesignShaurmaController {
     @GetMapping
     public String showDesignForm(Model model) {
         List<Ingredient> ingredients = Arrays.asList(
-                new Ingredient("WHBD", "White Bread", Ingredient.Type.WRAP),
-                new Ingredient("BLBD", "Black Bread", Ingredient.Type.WRAP),
-                new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN),
-                new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN),
-                new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES),
-                new Ingredient("TMTO", "Diced Tomato", Ingredient.Type.VEGGIES),
-                new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE),
-                new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE),
-                new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE),
-                new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE)
+                new Ingredient("WHBD", "White Bread", Type.WRAP),
+                new Ingredient("BLBD", "Black Bread", Type.WRAP),
+                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
+                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
+                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
+                new Ingredient("TMTO", "Diced Tomato", Type.VEGGIES),
+                new Ingredient("CHED", "Cheddar", Type.CHEESE),
+                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
+                new Ingredient("SLSA", "Salsa", Type.SAUCE),
+                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
         );
 
-
-        Ingredient.Type[] types = Ingredient.Type.values();
-
-        // TODO
-
-        for (Ingredient.Type type : types) {
+        Type[] types = Type.values();
+        for (Type type : types) {
             model.addAttribute(type.toString().toLowerCase(),
-                    filterByType(ingredients, type));
+                                filterByType(ingredients, type));
         }
 
-        model.addAttribute("design", new Taco());
+        model.addAttribute("design", new Shaurma());
 
         return "design";
+    }
+
+    private Object filterByType(List<Ingredient> ingredients, Type type) {
+        return ingredients.stream()
+                .filter(x -> x.getType().equals(type))
+                .collect(Collectors.toList());
     }
 
 
---
+++ src/main/java/com/shaurmacloud/entity/Shaurma.java
@@ -0,0 +1,12 @@
+package com.shaurmacloud.entity;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Shaurma {
+
+    private String name;
+    private List<String> ingredients;
+}
